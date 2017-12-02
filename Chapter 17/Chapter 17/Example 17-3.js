2768e4ee59c621abbd65e17c69a903d4527c6756
Fabien Thiombiano <thiombianofabien@gmail.com>
2017-12-02 17:26:48 -0600

    using jQuery UI - dialog plugin

---
+++ Chapter 17/Chapter 17/Example 17-3.js
@@ -0,0 +1,10 @@
+//jquery plugin - dialog box with styling
+
+$(document).ready(function(){
+
+    $("div#exampleDialog").dialog({
+
+        title: "Example Title" //setting the title
+
+    });
+});
