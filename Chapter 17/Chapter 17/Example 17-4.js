cf944f268bff2ae76e050302d157962e088bf786
Fabien Thiombiano <thiombianofabien@gmail.com>
2017-12-02 17:30:12 -0600

    using jQuery UI - dialog plugin - modal

---
+++ Chapter 17/Chapter 17/Example 17-4.js
@@ -0,0 +1,9 @@
+//jquery - plugin - setting a modal 
+
+$(document).ready(function(){
+
+    $("div#exampleDialog").dialog({
+        title: "Modal Title",
+        modal: true //activating modal
+    });
+});
